415c08e811578335a741c1b85a01fe248075ac0b
Shane Guan <xguan@andrew.cmu.edu>
2019-10-15 17:51:14 -0400

    val set

--- robust/model/mlp.py
+++ robust/model/mlp.py
@@ -12,6 +12,7 @@ import numpy as np
 EPS=1e-6
 GAMMA=.01
 JACRATE = .1
+WD = 0.005
 
 class Net(nn.Module):
   def __init__(self,numlayers=2):
@@ -36,9 +37,9 @@ class Net(nn.Module):
 
 def zerograd(model):
   for p in model.parameters():
-    p.grad-=p.grad
+    p.grad *= 0
   for z in model.zs:
-    z.grad -= z.grad
+    z.grad -= 0
 
 def trainFCN(args, model, device, train_loader, optimizer, epoch):
   model.train()
@@ -78,7 +79,7 @@ def trainFCN(args, model, device, train_loader, optimizer, epoch):
     #     epoch, batch_idx * len(data), len(train_loader.dataset),
     #     100. * batch_idx / len(train_loader), loss.item()))
 
-def test(args, model, device, test_loader):
+def test(args, model, device, test_loader,val=True):
   model.eval()
   test_loss = 0
   correct = 0
@@ -93,19 +94,24 @@ def test(args, model, device, test_loader):
       correct += pred.eq(target.view_as(pred)).sum().item()
 
   test_loss /= len(test_loader.dataset)
+  if val:
+    print('\nVal set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+      test_loss, correct, len(test_loader.dataset),
+      100. * correct / len(test_loader.dataset)))
+  else:
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+      test_loss, correct, len(test_loader.dataset),
+      100. * correct / len(test_loader.dataset)))
 
-  print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
 
 def main():
   # Training settings
   parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-  parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+  parser.add_argument('--batch-size', type=int, default=128, metavar='N',
             help='input batch size for training (default: 64)')
   parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
             help='input batch size for testing (default: 1000)')
-  parser.add_argument('--epochs', type=int, default=10, metavar='N',
+  parser.add_argument('--epochs', type=int, default=120, metavar='N',
             help='number of epochs to train (default: 10)')
   parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
             help='learning rate (default: 0.01)')
@@ -134,33 +140,45 @@ def main():
   device = torch.device("cuda" if use_cuda else "cpu")
 
   kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+  
+  imtransform = transforms.Compose([
+                   transforms.ToTensor(),
+                   transforms.Normalize((0.1307,), (0.3081,))
+                 ])
+
   train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../../data', train=True, download=True,
-             transform=transforms.Compose([
-               transforms.ToTensor(),
-               transforms.Normalize((0.1307,), (0.3081,))
-             ])),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
+                    datasets.MNIST('../../data', train=True, download=True,
+                        transform=imtransform),
+                    batch_size=args.batch_size, shuffle=True, **kwargs)
 
   test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../../data', train=False, transform=transforms.Compose([
-               transforms.ToTensor(),
-               transforms.Normalize((0.1307,), (0.3081,))
-             ])),
-    batch_size=args.test_batch_size, shuffle=True, **kwargs)
-
+                    datasets.MNIST('../../data', train=False, download=True,
+                        transform=imtransform),
+                    batch_size=args.test_batch_size, shuffle=False,**kwargs)
+  
+  val = torch.load("../../data/MNIST/processed/validate.pt")
+  val = list(val)
+  val[0] = [imtransform(i.numpy()) for i in val[0]]
+  val = list(zip(val[0],val[1]))
+  val_loader = torch.utils.data.DataLoader(val,batch_size=args.test_batch_size,shuffle=False,**kwargs)
 
   model = Net(args.numlayers).to(device)
   optimizer = optim.SGD(model.parameters(), 
-                        lr=args.lr, 
-                        momentum=args.momentum)
+                        lr=args.lr,
+                        momentum=args.momentum,
+                        weight_decay=WD if args.weight_decay else 0)
 
   for epoch in range(1, args.epochs + 1):
+    if epoch % 40 ==0:
+      print("decayed lr")
+      for param_group in optimizer.param_groups:
+        param_group['lr'] /= 10
     if args.large_margin:
       trainFCN(args, model, device, train_loader, optimizer, epoch)
     else: 
       train(args, model, device, train_loader, optimizer, epoch)
-    test(args, model, device, test_loader)
+    test(args, model, device, val_loader)
+  test(args, model, device, test_loader,val=False)
 
   if (args.save_model):
     torch.save(model.state_dict(),"mnist_cnn.pt")
