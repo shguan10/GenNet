e44e89e3d998eca79d5ee35c0956553ccd0d6f9b
Shane Guan <xguan@andrew.cmu.edu>
2019-10-14 22:45:37 -0400

    seems to work

--- robust/model/mlp.py
+++ robust/model/mlp.py
@@ -18,6 +18,10 @@ class Net(nn.Module):
     self.numlayers = 2
     self.layers = [nn.Linear(784,784) for _ in range(numlayers-1)]
     self.layers.append(nn.Linear(784,10))
+    self.params = nn.ParameterList([])
+    for l in self.layers:
+      self.params.extend(l.parameters())
+
   def forward(self, x):
     for layer in self.layers:
       x = F.relu(layer(x))
@@ -25,13 +29,15 @@ class Net(nn.Module):
 
 def zerograd(model):
   for p in model.parameters():
-    p.grad=torch.zeros(p.grad.shape)
+    p.grad-=p.grad
 
 def trainFCN(args, model, device, train_loader, optimizer, epoch):
   model.train()
-  # for batch_idx, (data, target) in tqdm(enumerate(train_loader)):
-  for (data, target) in tqdm((train_loader)):
+  for batch_idx, (data, target) in tqdm(enumerate(train_loader)):
+    # for (data, target) in tqdm((train_loader)):
     data, target = data.to(device), target.to(device)
+    bsize = data.shape[0]
+    data = data.reshape((bsize,784))
     optimizer.zero_grad()
     output = model(data)
     loss = F.nll_loss(output, target)
@@ -40,24 +46,29 @@ def trainFCN(args, model, device, train_loader, optimizer, epoch):
     lgs = [p.grad for p in model.parameters()]
     # zero the grad
     zerograd(model)
+    # pdb.set_trace()
     # calculate the Jacobian gradients as well
     # choose a random output index and calculate the gradient wrt that
     # for each layer l get the gradient
     # and take sum of products
-    chosen = torch.rand(len(data))
+    chosen = (torch.rand(len(data))*10).int()
     for bind,pi in enumerate(chosen):
       mask = torch.zeros(output.shape)
-      mask[pi]=1
-      output.backward(mask)
+      mask[bind,pi]=1
+      mask = mask.to(device)
+      output.backward(mask,retain_graph=bind<bsize)
       # if this is a simple FCN, the parameters come in pairs, each pair is for a layer, the first is the weights, the second is bias
-      for l in range(model.numlayers):
-        lgs[l*2] += 2 * model.layers[l].bias.grad.t() @ model.layers[l].bias.grad @ model.layers[l].weight
+      with torch.no_grad():
+        for l in range(model.numlayers):
+          bshape = model.layers[l].bias.shape[0]
+          lgs[l*2] += 2 * model.layers[l].bias.grad.reshape((bshape,1)) @ model.layers[l].bias.grad.reshape((1,bshape)) @ model.layers[l].weight
       zerograd(model)
     # update the gradients all-together and step
-    for p,g in zip(model.parameters,lgs):
+    for p,g in zip(model.parameters(),lgs):
       p.grad = g
 
     optimizer.step()
+    # if batch_idx % 100==0: print(loss.item())
     # if batch_idx % args.log_interval == 0:
     #   print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
     #     epoch, batch_idx * len(data), len(train_loader.dataset),
@@ -70,6 +81,8 @@ def test(args, model, device, test_loader):
   with torch.no_grad():
     for data, target in test_loader:
       data, target = data.to(device), target.to(device)
+      bsize = data.shape[0]
+      data = data.reshape((bsize,784))
       output = model(data)
       test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
       pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
@@ -106,6 +119,8 @@ def main():
 
   parser.add_argument('--large-margin', action='store_true', default=False, 
             help='use large-margin')
+  parser.add_argument('--numlayers', type=int, default=2, metavar='ls',
+            help='(default: 2)')
   args = parser.parse_args()
   use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -130,14 +145,14 @@ def main():
     batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
 
-  model = Net().to(device)
+  model = Net(args.numlayers).to(device)
   optimizer = optim.SGD(model.parameters(), 
                         lr=args.lr, 
                         momentum=args.momentum)
 
   for epoch in range(1, args.epochs + 1):
     if args.large_margin:
-      train_FCN(args, model, device, train_loader, optimizer, epoch)
+      trainFCN(args, model, device, train_loader, optimizer, epoch)
     else: 
       train(args, model, device, train_loader, optimizer, epoch)
     test(args, model, device, test_loader)
